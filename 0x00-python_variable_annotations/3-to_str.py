c61bec01b815f73a7a8255249c76e9d582a6d492
Ukeme Edet <ukemeedet2207@gmail.com>
2024-10-10 19:53:08 +0100

    Add type annotations to the to_str function

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module contains the function for task 3
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a float to a string.
+
+    Args:
+        n (float): a float number
+
+    Returns:
+        str: the string representation of the float
+    """
+    return str(n)
