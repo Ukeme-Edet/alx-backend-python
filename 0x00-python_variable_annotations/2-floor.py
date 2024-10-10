11e52e61a6a3223b4372a4a8e4c401ba10daea4c
Ukeme Edet <ukemeedet2207@gmail.com>
2024-10-10 19:52:59 +0100

    Add type annotations to the floor function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module contains the function for task 2
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a float
+
+    Args:
+        n (float): a float number
+
+    Returns:
+        int: the floor of the float
+    """
+    return int(n)
