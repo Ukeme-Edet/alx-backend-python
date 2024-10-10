e644be3f47cc175335c2bd4f5498c654cee1f6c4
Ukeme Edet <ukemeedet2207@gmail.com>
2024-10-10 19:57:23 +0100

    Add sum_list function with type annotations

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module contains the function for task 5
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Returns the sum of the list of floats
+
+    Args:
+            input_list (List[float]): a list of floats
+
+    Returns:
+            float: the sum of the list of floats
+    """
+    return sum(input_list)
