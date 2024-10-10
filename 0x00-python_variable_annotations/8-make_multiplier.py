c6ebb93eedc10823d64aceae3f09c84888a9caa9
Ukeme Edet <ukemeedet2207@gmail.com>
2024-10-10 20:33:38 +0100

    Refactor make_multiplier function to adhere to PEP8 line length guidelines

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,23 +1,9 @@
 #!/usr/bin/env python3
-"""
-This module contains a function that creates a multiplier function.
-
-The `make_multiplier` function takes a float `multiplier` as an argument
-and returns a function that multiplies its input by the given multiplier.
+"""Task 8's module.
 """
 from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """
-    make_multiplier - Returns a function that multiplies a given float by a\
-        specified multiplier.
-
-    Args:
-        multiplier (float): The multiplier to use in the returned function.
-
-    Returns:
-        Callable[[float], float]: A function that takes a float and returns\
-            the product of it and the multiplier.
-    """
+    """Creates a multiplier function."""
     return lambda x: x * multiplier
