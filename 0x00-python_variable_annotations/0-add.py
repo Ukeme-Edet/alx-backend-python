d89d65a7ec8f035d9c940ef04e5d1ae144432fcb
Ukeme Edet <ukemeedet2207@gmail.com>
2024-10-10 19:40:59 +0100

    Refactor add function with type annotations and update docstring

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
-0-add.py: Write a type-annotated function add that takes a float a and a float
-b as arguments and returns their sum as a float.
+0-add.py: Write a type-annotated function add that takes a float a and a float\
+    b as arguments and returns their sum as a float.
 """
 
 
