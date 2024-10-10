938f0ac270aab0c5807248c65e7e5e4ba6051fd8
Ukeme Edet <ukemeedet2207@gmail.com>
2024-10-10 20:15:38 +0100

    Refactor to_kv function to convert key and value to a tuple with squared value

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -7,13 +7,14 @@ from typing import Tuple, Union
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    Returns a tuple of a string and the sqrt of v
+    Converts a key and a value to a tuple where the key is a string and the\
+        value is the square of the original value.
 
     Args:
-        k (str): The string k
-        v: (Union[int, float]): The number to be rooted
+        k (str): The key.
+        v (Union[int, float]): The value to be squared.
 
     Returns:
-        Tuple[str, float]: A tuple of the string and rooted number
+        Tuple[str, float]: A tuple containing the key and the squared value.
     """
     return (k, v**0.5)
