990d1e20fb859a040baa92a64ae375ba26ae948f
Ukeme Edet <ukemeedet2207@gmail.com>
2024-10-11 02:01:54 +0100

    Refactor zoom_array function to support zooming in on a tuple by a given factor

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,13 @@
+from typing import List, Tuple
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    zoomed_in: list = [item for item in lst for i in range(factor)]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
